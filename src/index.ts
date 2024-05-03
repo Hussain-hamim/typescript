64c96f84df1e50c9e3da63b7031ac6f13b3487c1
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-03 18:42:07 +0430

    object in ts.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-function calculateTax(income: number, taxYear = 2022): number {
-  // let x;
-  if (taxYear < 2022) return income * 1.2;
-  return income * 1.3;
-}
+//object:
 
-calculateTax(11_000);
+//we can it make optional
+let employee: {
+  id: number;
+  name?: string;
+} = {
+  id: 1,
+};
+employee.name = "Hussain";
+
+//we can initialize it to empty string
+let employee2: {
+  id: number;
+  name: string;
+  readonly age: number;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: "",
+  age: 22,
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+employee2.name = "Hussain";
+// employee2.age = 222;
