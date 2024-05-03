791b1ba564267129b328e0031917aaac3269e8ff
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-03 11:05:30 +0430

    the any type....

--- src/index.ts
+++ src/index.ts
@@ -10,26 +10,9 @@ let sale: number = 123_456_789;
 let course: string = "TypeScript";
 let is_published: boolean = true;
 let level;
+level = 1;
+level = "a";
 
-//exercise:
-let a = 100;
-let b = "coffee";
-let c = [true, false, false];
-let d = { age: 20 };
-let e = [3];
-let f;
-let g = [];
-
-interface Song {
-  title: string;
-  releaseYear: number;
-}
-
-let song: Song = { title: "my song", releaseYear: 2002 };
-
-let prices = [100, 200, 300];
-prices[0] = 1100;
-
-function myFunc(a: number, b: number): number {
-  return a + b;
+function render(document: any) {
+  console.log(document);
 }
