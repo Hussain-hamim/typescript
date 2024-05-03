da02144107bc26624dc14b772a71ecdea6a7c459
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-03 11:16:41 +0430

    array in ts.

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@
 let sale: number = 123_456_789;
 let course: string = "TypeScript";
 let is_published: boolean = true;
+//any
 let level;
 level = 1;
 level = "a";
@@ -16,3 +17,11 @@ level = "a";
 function render(document: any) {
   console.log(document);
 }
+
+//array
+let numbers: number[] = [1, 2, 3];
+let numbers2 = [1, 2, 3];
+
+//intellisense
+let numbers3: number[] = [];
+numbers3.forEach((n) => n.toLocaleString);
