bec8561f6074ad79c63cef02b7fc1c6aaec88c5d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-04 20:17:16 +0430

    Nullish coalescing operator in ts.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,9 @@
-type Customer = {
-  birthday?: Date;
+let speed: number | null = null;
+
+let ride = {
+  // falsy: (undefined, null, '', false, 0)
+  // speed: speed !== null ? speed : 30,
+
+  // Nullish coalescing operator(equal to line 5)
+  speed: speed ?? 30,
 };
-
-function getCustomer(id: number): Customer | null {
-  return id == 0 ? null : { birthday: new Date() };
-}
-
-let customer = getCustomer(0);
-//we check or...
-if (customer !== null) console.log(customer.birthday);
-//we use optional chaining.
-
-//(?.): 1. optional property access operator
-console.log(customer?.birthday);
-
-let customer2 = getCustomer(1);
-console.log(customer2?.birthday?.getFullYear());
-
-// 2. optional element access operator: we this in array
-//customer?.[0]
-
-// 3. optional call
-let log: any = null;
-log?.("a");
