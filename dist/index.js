59b9bde84d5115295f79c34fb32b3a06bd9a6f32
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-04 21:25:09 +0430

    working with unknown type in ts

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,9 @@
 "use strict";
-var _a;
-function getCustomer(id) {
-    return id == 0 ? null : { birthday: new Date() };
+function render(document) {
+    if (typeof document === "string") {
+        console.log(document);
+        document.toUpperCase();
+    }
 }
-let customer = getCustomer(0);
-if (customer !== null)
-    console.log(customer.birthday);
-console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
-let customer2 = getCustomer(1);
-console.log((_a = customer2 === null || customer2 === void 0 ? void 0 : customer2.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+render("Hussain");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ function render(document: unknown) {
   //Narrowing
   if (typeof document === "string") {
     document.toUpperCase();
+    console.log(document);
   }
   // document.move();
   // document.fly();
 }
+render("Hussain");
