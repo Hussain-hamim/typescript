b55821e8b5f3c4d0d3560e6fe274b8de87191b56
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-04 14:26:23 +0430

    type aliases.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,17 @@
 //object:
-
-//we can it make optional
-let employee: {
-  id: number;
-  name?: string;
-} = {
-  id: 1,
-};
-employee.name = "Hussain";
-
-//we can initialize it to empty string
-let employee2: {
-  id: number;
+//type aliases: show shape of our object.
+type Employee = {
+  readonly id: number;
   name: string;
-  readonly age: number;
   retire: (date: Date) => void;
-} = {
+};
+
+let employee: Employee = {
   id: 1,
-  name: "",
-  age: 22,
+  name: "Hussain",
   retire: (date: Date) => {
     console.log(date);
   },
 };
-employee2.name = "Hussain";
-// employee2.age = 222;
+
+let emp = { ...employee };
