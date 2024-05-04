8e3d9975cdad92e07a20b5989498f1460cb562ac
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-04 15:50:10 +0430

    nullable type in typescript.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-//literal type (exact. specific) value:
+// Nullable type:
+// we can use with union type
+function greet(name: string | null | undefined) {
+  if (name) console.log(name.toUpperCase());
+  else console.log("Hola!");
+}
 
-let quantity: 50 | 100 = 50;
-
-type Quantity = 100 | 500;
-let quantity2: Quantity = 500;
-
-type Metric = "cm" | "inch";
-
-// let metric: Metric = 'hussain';//error
-let metric2: Metric = "cm";
+greet(null);
