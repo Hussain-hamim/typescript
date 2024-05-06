c1169656a12d3b0af10f362e6a807fd56214d004
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-06 16:41:21 +0430

    readonly and optional property

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 // classes in Typescript:
 class Account {
-  id: number;
+  readonly id: number;
   owner: string;
   balance: number;
+  nickname?: string; //this is optional
 
   constructor(id: number, owner: string, balance: number) {
     this.id = id;
@@ -15,11 +16,13 @@ class Account {
     this.balance += amount;
   }
 }
-
+//deposit mean put mean in bank account and withdraw mean take out money from bank
 //object:
 let account = new Account(1, "Hussain", 0);
 account.deposit(100);
 console.log(account.balance);
+// account.id = 0; //read only can not be assign
+
 console.log(account);
 console.log(typeof account);
 console.log(account instanceof Account);
