f565512eeaa2eface19a41441ad7b2734ca1e23b
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-06 16:27:40 +0430

    creating object the previous class.

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,11 @@ class Account {
     this.balance += amount;
   }
 }
+
+//object:
+let account = new Account(1, "Hussain", 0);
+account.deposit(100);
+console.log(account.balance);
+console.log(account);
+console.log(typeof account);
+console.log(account instanceof Account);
