1c8168ef25e72725b57a888e6d3c430a1f0acadb
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-06 22:03:02 +0430

    knowing static members in ts

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,16 @@
 "use strict";
-class Account {
-    constructor(id, owner, _balance) {
-        this.id = id;
-        this.owner = owner;
-        this._balance = _balance;
+class Ride {
+    start() {
+        Ride.activeRides++;
     }
-    deposit(amount) {
-        if (amount <= 0)
-            throw new Error("Invalid amount");
-        this._balance += amount;
-    }
-    get balance() {
-        return this._balance;
-    }
-    set balance(value) {
-        if (value < 0)
-            throw new Error("Invalid value...");
-        this._balance = value;
+    stop() {
+        Ride.activeRides--;
     }
 }
-let account = new Account(1, "Hussain", 0);
-account.deposit(100);
-console.log(account.balance);
-account.balance = 1;
-console.log(account.balance);
-console.log(account);
-console.log(typeof account);
-console.log(account instanceof Account);
+Ride.activeRides = 0;
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.start();
+console.log(Ride.activeRides);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
-// class person {};
-// person.name = "a"; // dynamically add values to object: this is possible in javascript
+//************Static Members:
+class Ride {
+  //with static this property now belong to the class not the object(this)
+  private static _activeRides: number = 0;
 
-//but in typescript we use index signature way to do so:
-class SeatAssignment {
-  // index signature property: dynamically add values to object in ts
-  [seatNumber: string]: string;
+  // static activeRides: number = 0;
+  // start(){this.activeRides++; }
+
+  // start(){this.activeRides++; }
+  // now replace (this) with class name
+  start() {
+    Ride._activeRides++;
+  }
+  stop() {
+    Ride._activeRides--;
+  }
+  // when we make a property or method static they become part of the class and will have only a single instance in memory
+  static get activeRides() {
+    return Ride._activeRides;
+  }
 }
 
-let seats = new SeatAssignment();
-seats.A1 = "Hussain";
-seats["A2"] = "Hamim";
-// seats.A3 = 1; // give type error
+// Ride.activeRides = 10; //it's readonly cuz it's static
+
+let ride1 = new Ride();
+ride1.start();
+
+let ride2 = new Ride();
+ride2.start();
+
+// now the activeRides property live on class only not object cuz it's static
+console.log(Ride.activeRides);
