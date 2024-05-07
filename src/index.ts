2a4fb79ebdf3a8430a963633bb048eb8f38b016a
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-07 21:31:37 +0430

    getting know abstract classes and methods.

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,22 @@
-////Inheritance:
-class Person {
-  constructor(public firstName: string, public lastName: string) {}
+// Abstract class and method:
+// abstract class mean it have to be extended by other
+abstract class Shape {
+  constructor(public color: string) {}
 
-  get fullName() {
-    return this.firstName + " " + this.lastName;
+  //// the abstract method in abstract class do not have implementation.
+  abstract render(): void;
+}
+
+class Circle extends Shape {
+  constructor(public radius: string, color: string) {
+    super(color);
   }
 
-  protected walk() {
-    console.log("Walking...");
+  override render(): void {
+    console.log("rendering a circle...");
   }
 }
 
-class Student extends Person {
-  constructor(public studentId: number, firstName: string, lastName: string) {
-    super(firstName, lastName);
-  }
-
-  takeTest() {
-    // can be inherited cuz it's 'protected'
-    this.walk();
-    console.log("Taking a test...");
-  }
-}
-
-// let student = new Student(1, "Hussain", "Hamim");
-// student.walk;
-
-class Teacher extends Person {
-  override get fullName() {
-    return "Professor " + super.fullName;
-  }
-}
-
-let teacher = new Teacher("Hussain", "Hamim");
-// console.log(teacher.fullName);
-
-class Principal extends Person {
-  override get fullName() {
-    return "Principal " + super.fullName;
-  }
-}
-
-//// Polymorphism:
-printNames([
-  new Student(1, "Hussain", "Hamim"),
-  new Teacher("Haroon", "Khan"),
-  new Principal("Hamza", "Khan"),
-]);
-
-function printNames(people: Person[]) {
-  for (let person of people) {
-    console.log(person.fullName);
-  }
-}
-////Class
-//// Open Closed Principle:
-//class should be Open for extension and Closed for modification.
-
-// Private and Protected members:
-//both are the same we can only access inside our class
-// but the difference is that protected member can be inherited
+//// it do not make sense
+// let shape = new Shape("red"); // cannot create instance of abstract class.
+// shape.render();
