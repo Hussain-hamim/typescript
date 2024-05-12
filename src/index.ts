2b576caebb7795fa0a6efafc4d34a9f48b70da8e
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-12 14:43:40 +0430

    generic classes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-class keyValuePairs {
-  constructor(public key: number, public value: string) {}
-}
-class StringkeyValuePairs {
-  constructor(public key: string, public value: string) {}
+class keyValuePairs<K, V> {
+  constructor(public key: K, public value: V) {}
 }
 
-let pair = new keyValuePairs(1, "Hussain");
-let pair2 = new StringkeyValuePairs("1", "Hussain");
+let pair = new keyValuePairs<number, string>(1, "Hussain");
+let pair2 = new keyValuePairs<string, string>("A", "Hussain");
+//// we can ignore supplying generic type, the compiler can infer:
+let pair3 = new keyValuePairs("A", "Hussain");
