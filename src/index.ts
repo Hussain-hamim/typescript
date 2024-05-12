24944d0eedcad28fb246c34ac2a9c3569f2ac739
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-12 17:28:18 +0430

    working with Extending generic classes

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,37 @@
-///// constraints:
-
-// constraints by types
-function echo<T extends number | string>(value: T) {
-  return value;
-}
-echo(1);
-echo("a");
-
-/// constraints by object
-//(generic type parameter)
-function echo2<T extends { name: string }>(value: T) {
-  return value;
-}
-echo2({ name: "hussain" });
-
-//// constraints by interface
-interface Person {
+// Extending generic classes:
+interface Product {
   name: string;
-  age: number;
+  price: number;
 }
-function echo3<T extends Person>(value: T) {
-  return value;
-}
-echo3({ name: "hamim", age: 22 });
-// echo3({ name: "hamim" });
 
-/// constraints by class
-class Person2 {
-  constructor(public name: string) {}
+class Store<T> {
+  protected _object: T[] = [];
+
+  add(obj: T): void {
+    this._object.push(obj);
+  }
 }
-class Customer extends Person2 {}
-function echo4<T extends Person2>(value: T) {
-  return value;
+// let store = new Store<Product>();
+
+//// pass on the generic type parameter
+class CompressibleStore<T> extends Store<T> {
+  compress() {}
 }
-echo4({ name: "hamim" });
-echo4(new Person2("hamim"));
-echo4(new Customer("afghan"));
+
+let store = new CompressibleStore<Product>();
+store.add({ name: "Hamim", price: 3 });
+store.compress();
+
+// restrict the generic type parameter
+class SearchableStore<T extends { name: string }> extends Store<T> {
+  find(name: string): T | undefined {
+    return this._object.find((obj) => obj.name === name);
+  }
+}
+
+// fix the generic type parameter
+class ProductStore extends Store<Product> {
+  filterByCategory(category: string): Product[] {
+    return [];
+  }
+}
