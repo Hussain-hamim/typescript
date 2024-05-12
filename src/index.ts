1d7d5b0df40f8258424db0c9d3d586d868c7d369
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-12 14:53:13 +0430

    generic function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-class keyValuePairs<K, V> {
-  constructor(public key: K, public value: V) {}
+///// generic function;
+function wrapInArray<T>(value: T) {
+  return [value];
 }
+let numbers = wrapInArray("A");
+let numbers2 = wrapInArray(1);
 
-let pair = new keyValuePairs<number, string>(1, "Hussain");
-let pair2 = new keyValuePairs<string, string>("A", "Hussain");
-//// we can ignore supplying generic type, the compiler can infer:
-let pair3 = new keyValuePairs("A", "Hussain");
+// inside class
+class ArrayUtils {
+  wrapInArray2<T>(value: T) {
+    return [value];
+  }
+}
+let utils = new ArrayUtils();
+let numbers3 = utils.wrapInArray2(1);
+let numbers4 = utils.wrapInArray2("A");
+
+//make it static
+class ArrayUtils2 {
+  static wrapArray<T>(value: T) {
+    return [value];
+  }
+}
+let numbers5 = ArrayUtils2.wrapArray(1);
+let numbers6 = ArrayUtils2.wrapArray("B");
