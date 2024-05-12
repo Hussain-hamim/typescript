e77f28749c09f5c2f4b4c781dac5aca5804d725a
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-12 14:35:13 +0430

    understanding the problem.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
-const input = prompt("what fruit do you like?");
-const css = "color: #ffffff";
-
-switch (input) {
-  case "orange": {
-    const css = "color: #FFA500";
-    console.log("%orange!", css);
-    break;
-  }
-  case "lemon":
-    const css = "color: #FFFF00";
-    console.log("%yellow!", css);
-    break;
-  default:
-    console.log("no color for you");
+class keyValuePairs {
+  constructor(public key: number, public value: string) {}
 }
+class StringkeyValuePairs {
+  constructor(public key: string, public value: string) {}
+}
+
+let pair = new keyValuePairs(1, "Hussain");
+let pair2 = new StringkeyValuePairs("1", "Hussain");
