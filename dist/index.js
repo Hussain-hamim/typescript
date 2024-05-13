c93dc0654eeabb8f609fed856df3cada2bf4a62f
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-13 16:44:58 +0430

    enhance method using method decorator.

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Log(target, hey, descriptor) {
+function Log(target, methodName, descriptor) {
     const original = descriptor.value;
     descriptor.value = function (...args) {
         console.log("before");
@@ -18,10 +18,16 @@ class Person {
     say(message) {
         console.log("person say " + message);
     }
+    tell(message) {
+        console.log("hello there " + message);
+    }
 }
 __decorate([
     Log
 ], Person.prototype, "say", null);
+__decorate([
+    Log
+], Person.prototype, "tell", null);
 const person = new Person();
-person.say("Hello");
+person.tell("Hello");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
 function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
-  const original = descriptor.value as Function;
+  const original = descriptor.value as Function; //=> type assertion
   // we can't use arrow function, when we redefine our method we use function declaration.
   descriptor.value = function (...args: any) {
+    // the value property reference the target method
     console.log("before");
     original.call(this, "blue sky");
     original.call(this, ...args);
     console.log("after");
   };
 }
-//oil, tomato, potato
+//oil, tomato, potato => call bro
 class Person {
   @Log
   say(message: string) {
     console.log("person say " + message);
   }
+
+  @Log
+  tell(message: string) {
+    console.log("hello there " + message);
+  }
 }
 
 const person = new Person();
-person.say("Hello");
+person.tell("Hello");
