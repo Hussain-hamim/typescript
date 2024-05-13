abae15e6bf6152adf21353ca8cc649fdb371484d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-13 10:04:24 +0430

    knowing type mapping in ts

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,25 @@
-// Extending generic classes:
+//// type mapping:
 interface Product {
   name: string;
   price: number;
 }
 
-class Store<T> {
-  protected _object: T[] = [];
+type readOnlyProduct = {
+  //index signature, //keyof
+  readonly [K in keyof Product]: Product[K]; // type mapping
+};
 
-  add(obj: T): void {
-    this._object.push(obj);
-  }
-  // if T is Product then
-  // keyof T => 'name' | 'price'
-  find(property: keyof T, value: unknown): T | undefined {
-    return this._object.find((obj) => obj[property] === value);
-  }
-}
-let store3 = new Store<Product>();
-store3.add({ name: "a", price: 1 });
-store3.find("name", "a");
-store3.find("name", 1);
-store3.find("price", 1);
-// store3.find("notExistingProperty", 1); // compile time error
+// this is generic not just for product:
+type Optional<T> = {
+  [K in keyof T]?: T[K];
+};
 
-// let store = new Store<Product>();
+type Nullable<T> = {
+  [K in keyof T]: T[K] | null;
+};
 
-//// pass on the generic type parameter
-class CompressibleStore<T> extends Store<T> {
-  compress() {}
-}
-
-let store = new CompressibleStore<Product>();
-store.add({ name: "Hamim", price: 3 });
-store.compress();
-
-// restrict the generic type parameter
-class SearchableStore<T extends { name: string }> extends Store<T> {
-  find(name: string): T | undefined {
-    return this._object.find((obj) => obj.name === name);
-  }
-}
-
-// fix the generic type parameter
-class ProductStore extends Store<Product> {
-  filterByCategory(category: string): Product[] {
-    return [];
-  }
-}
+let product: readOnlyProduct = {
+  name: "a",
+  price: 1,
+};
+// product.name = 'b'; //can not assign cuz it's readonly
