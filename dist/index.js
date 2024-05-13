d50ae3009eb1f8468b7b0128ee9847be1b0ff10a
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-13 18:45:12 +0430

    the THIS with .call(), .bind(), .apply()

--- dist/index.js
+++ dist/index.js
@@ -1,33 +1,8 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-function Log(target, methodName, descriptor) {
-    const original = descriptor.value;
-    descriptor.value = function (...args) {
-        console.log("before");
-        original.call(this, "blue sky");
-        original.call(this, ...args);
-        console.log("after");
-    };
+function greet(text = "welcome") {
+    console.log(`${text} ${this.username}`);
 }
-class Person {
-    say(message) {
-        console.log("person say " + message);
-    }
-    tell(message) {
-        console.log("hello there " + message);
-    }
-}
-__decorate([
-    Log
-], Person.prototype, "say", null);
-__decorate([
-    Log
-], Person.prototype, "tell", null);
-const person = new Person();
-person.tell("Hello");
+greet.call({ username: "Hussain" });
+greet.bind({ username: "Hamim" })();
+greet.apply({ username: "afghan" }, ["salaam"]);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,36 @@
-function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
-  const original = descriptor.value as Function; //=> type assertion
-  // we can't use arrow function, when we redefine our method we use function declaration.
-  descriptor.value = function (...args: any) {
-    // the value property reference the target method
-    console.log("before");
-    original.call(this, "blue sky");
-    original.call(this, ...args);
-    console.log("after");
-  };
-}
-//oil, tomato, potato => call bro
-class Person {
-  @Log
-  say(message: string) {
-    console.log("person say " + message);
-  }
+// function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
+//   const original = descriptor.value as Function; //=> type assertion
+//   // we can't use arrow function, when we redefine
+//   // our method we use function declaration. and also THIS in arrow func
+//   // refer to lexical environment(define where the arrow func define).
+//   descriptor.value = function (...args: any) {
+//     // the value property reference the target method
+//     console.log("before");
+//     original.call(this, "blue sky");
+//     original.call(this, ...args);
+//     console.log("after");
+//   };
+// }
+// //oil, tomato, potato => call bro
+// class Person {
+//   @Log
+//   say(message: string) {
+//     console.log("person say " + message);
+//   }
 
-  @Log
-  tell(message: string) {
-    console.log("hello there " + message);
-  }
+//   @Log
+//   tell(message: string) {
+//     console.log("hello there " + message);
+//   }
+// }
+
+// const person = new Person();
+// person.tell("Hello");
+
+function greet(this: any, text = "welcome") {
+  console.log(`${text} ${this.username}`);
 }
 
-const person = new Person();
-person.tell("Hello");
+greet.call({ username: "Hussain" });
+greet.bind({ username: "Hamim" })();
+greet.apply({ username: "afghan" }, ["salaam"]);
