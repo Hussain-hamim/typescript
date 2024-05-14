259491747e37c8bffe129098e61be9a189d0ed02
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-14 18:12:04 +0430

    wild card import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Circle, Square } from "./shapes";
 import Store, { Format } from "./storage";
+import * as Shapes from "./shapes"; // wild card import
+
+let circ = new Shapes.Circle(1);
+console.log(circ);
 
 let circle = new Circle(4);
 let square = new Square(4);
