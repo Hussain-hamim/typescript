aa971017332b80ddb8a17eff0cf146909f68c7f4
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-14 18:09:22 +0430

    default export in ts.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Circle, Square } from "./shapes";
+import Store, { Format } from "./storage";
 
 let circle = new Circle(4);
 let square = new Square(4);
 console.log(circle);
 console.log(square);
+console.log(Store);
+console.log(Format);
