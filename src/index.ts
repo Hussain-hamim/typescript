0e7cecff30762d42cfc1bca5bf28ce60a919c44a
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-14 18:23:09 +0430

    working with re-exporting in modules.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import { Circle, Square } from "./shapes";
-import Store, { Format } from "./storage";
-import * as Shapes from "./shapes"; // wild card import
-
-let circ = new Shapes.Circle(1);
-console.log(circ);
-
-let circle = new Circle(4);
-let square = new Square(4);
-console.log(circle);
-console.log(square);
-console.log(Store);
-console.log(Format);
+import { Square, Circle } from "./shapes";
---
+++ src/shapes/index.ts
@@ -0,0 +1,5 @@
+// this is called re-exporting
+export { Circle } from "./Circle";
+export { Square } from "./Square";
+
+// export { Circle, Square };
