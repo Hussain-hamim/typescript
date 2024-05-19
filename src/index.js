34d4bd6079c63b8dae44acf07182cbcad880fd3f
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-19 21:57:15 +0430

    getting know better properties.

--- src/index.js
+++ src/index.js
@@ -1,169 +1,20 @@
-// // show the concept of an unintentionally missing value
+// Chapter 8: properties:
 
-// let bandersnatch;
-// console.log(bandersnatch);
+let sherlock = {
+  surname: "Holmes",
+  address: { city: "London" },
+};
 
-// // null: used for intentionally missing value.
-// let mimsy = null;
-// console.log(mimsy);
+let john = {
+  surname: "Watson",
+  address: sherlock.address,
+};
 
-// console.log(typeof null);
+john.surname = "Lennon";
+john.address.city = "Malibu";
 
-// console.log(Number.MAX_SAFE_INTEGER);
-// console.log(Number.MIN_SAFE_INTEGER);
+console.log(sherlock.surname);
+console.log(sherlock.address.city);
 
-// console.log(Number.MAX_SAFE_INTEGER + 1);
-// console.log(Number.MAX_SAFE_INTEGER + 2);
-// console.log(Number.MAX_SAFE_INTEGER + 3);
-// console.log(Number.MAX_SAFE_INTEGER + 4);
-// console.log(Number.MAX_SAFE_INTEGER + 5);
-
-// console.log(0.0 + 0.2 === 0.30000000000000004);
-
-// console.log(1 / (-1 / 0));
-
-// console.log(typeof (0 / 0));
-
-// console.log(typeof NaN);
-
-// console.log(typeof "こんにちは"); // "string"
-// console.log(typeof "こんにちは"); // "string"
-// console.log(typeof `こんにちは`); // "string"
-
-// let cat = "Cheshire";
-
-// console.log(cat.length);
-// console.log(cat[0]);
-// console.log(cat[1]);
-
-// let answer = prompt("enter your name");
-// console.log(answer);
-
-// let alo = Symbol();
-// console.log(typeof alo);
-
-// let junk = {};
-// junk = null;
-
-// console.log(junk);
-
-// for (let i = 0; i < 7; i++) {
-//   console.log(function () {});
-// }
-
-// let count = function () {
-//   return 7;
-// };
-// let c = count;
-// console.log(c);
-
-// let a = "1";
-// let b = 1;
-
-// console.log(a === b);
-// console.log(a === b);
-// console.log(Object.is(a, b));
-
-// const c = { a: "Hello" };
-
-// console.log(Object.is(c, c));
-// console.log(c == c);
-
-// let dwarves = 7;
-// let continents = "7";
-// let worldWonders = 3 + 4;
-
-// console.log(Object.is(dwarves, worldWonders));
-// // console.log(dwarves === continents);
-
-// let banana = { a: "Hussain" };
-// let cherry = banana;
-// let chocolate = cherry;
-// cherry = { b: "Hamim" };
-
-// console.log(Object.is(banana, cherry)); // false
-// console.log(Object.is(cherry, chocolate)); // false
-// console.log(Object.is(chocolate, banana)); // true
-
-// console.log(chocolate.a);
-// console.log(cherry.b);
-
-// Equality in js:
-// Strict Equality: a === b (triple equals).
-// Loose Equality: a == b (double equals).
-// Same Value Equality: Object.is(a, b) .
-
-// strictly equality:
-// console.log(2 === 2);
-// console.log({} === {});
-
-// strict value equality vs. same value equality:
-// both of them are the same in almost all cases
-// some exception:
-// console.log(NaN === NaN); // false
-// console.log(-0 === 0); // true
-
-// // case 1:
-// let width = 0 / 0; // NaN
-
-// let height = width * 2; // NaN
-
-// console.log(width === height); // false
-// console.log(Object.is(width, height)); // true
-
-// function resizeImage(size) {
-//   if (size !== size) {
-//     // This will never get logged: the check is always false!
-//     // but there are some solution like:
-//     // Number.isNaN(size);
-//     // object.is(size, NaN);
-//     // size !== size;
-//     console.log("Something is wrong.");
-
-//     console.log(size !== size);
-//   } else console.log("okey...");
-// }
-
-// resizeImage(NaN);
-
-// special case two: -0
-
-// let width = 0;
-// let height = -width;
-// console.log(width === height); // true
-
-// console.log(Object.is(width, height)); //false
-
-////////////////////
-
-// function strictEqual(a, b) {
-//   if (a === b) {
-//     return console.log(a + b);
-//   }
-// }
-// strictEqual(2, 2);
-
-// //////////////
-// // Loose equality: ==
-// // not so much common so it's best to avoid it.
-
-// console.log([[]] == "");
-// console.log(true == [1]);
-// console.log(false == [0]);
-
-// let x = null;
-
-// // here even if the x is null or undefined it is the same
-// if (x == null) {
-//   console.log("hello there bro");
-// }
-
-// let y = undefined;
-// if (y == null) {
-//   // (x === null || x === undefined)
-//   console.log("Mmm awesome");
-// }
-
-let x = 0 / 0;
-
-console.log(Number.isNaN(x));
+console.log(john.surname);
+console.log(john.address.city);
