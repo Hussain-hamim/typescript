67a0c75f5dca63c6644bf15b9398da7fa5aba2b7
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-20 08:59:33 +0430

    recap of properties

--- src/index.js
+++ src/index.js
@@ -27,3 +27,21 @@ console.log(john.address.city);
 console.log(john.address.city === sherlock.address.city);
 /// The rules say that null or undefined on the le side is an error.
 console.log(sherlock.boat.name);
+
+// Recap:
+//  Properties are wires—a bit like variables. They both point to values. Unlike
+//  variables, properties start from objects in our universe.
+//  Properties belong to particular objects. You can’t have more than one
+//  property with the same name on an object.
+//  Generally, you can perform an assignment in three steps:
+
+//  Figure out which wire is on the le .
+//  Figure out which value is on the right.
+//  Point that wire to that value.
+//  An expression like
+// obj.property is calculated in three steps:
+// Figure out which value is on the le .
+
+//  If it’s null or undefined , throw an error.
+//  If that property exists, the result is the value it points to. If that property
+//  doesn’t exist, the result is undefined .
