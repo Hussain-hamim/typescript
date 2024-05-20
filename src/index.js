fc83322bd77cf7af78a375bc4733a5ecb91e0eb0
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-20 08:51:28 +0430

    th complexity of properties.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ let john = {
 };
 
 john.surname = "Lennon";
+sherlock.address.city = "Khost";
 john.address.city = "Malibu";
 
 console.log(sherlock.surname);
@@ -18,3 +19,11 @@ console.log(sherlock.address.city);
 
 console.log(john.surname);
 console.log(john.address.city);
+
+// let me = { surname: "Hussain", age: 22 };
+// let name = prompt("what is your name?");
+// alert(me[name]);
+
+console.log(john.address.city === sherlock.address.city);
+/// The rules say that null or undefined on the le side is an error.
+console.log(sherlock.boat.name);
