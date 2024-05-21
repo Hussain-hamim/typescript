b21018c3c8a25844ab9950200e1f2e25420174d7
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-05-21 07:45:43 +0430

    clear ups...

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// let vs const:
-const shrek = { species: "ogre" };
-// const value are read-only, so we can't point to a different value.
-// shrek = "fiona"; // type error
-// but we can mutate the object our wire point to
-shrek.species = "human";
-console.log(shrek.species);
-
-// remember that const prevents variable reassignment - not object mutation
